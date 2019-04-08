5199d7b8483305fa997163607df0662b5be19d27
Marcin Kotz <marcin@kotz.pl>
2019-04-08 21:10:14 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mini test
+
+To check for dependency updates:
+
+    mvn versions:display-dependency-updates
---
+++ src/com/er453r/MiniMain.java
@@ -0,0 +1,7 @@
+package com.er453r;
+
+public class MiniMain {
+    public static void main(String[] args) {
+        System.out.println("Hello derp!");
+    }
+}
