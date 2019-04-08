26b92c4c9766d6358f6d4d8a373a798b61d4a1ec
Marcin Kotz <marcin@kotz.pl>
2019-04-09 00:03:42 +0200

    simple project works

--- src/com/er453r/MiniMain.java
+++ src/com/er453r/MiniMain.java
@@ -1,7 +1,23 @@
 package com.er453r;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class MiniMain {
     public static void main(String[] args) {
         System.out.println("Hello derp!");
+
+        SpringApplication.run(MiniMain.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(name="name", required=false, defaultValue="General Kenobi") String name) {
+        return String.format("Hello there %s", name);
     }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,7 @@
 module mini {
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.web;
+
+    opens com.er453r;
 }
